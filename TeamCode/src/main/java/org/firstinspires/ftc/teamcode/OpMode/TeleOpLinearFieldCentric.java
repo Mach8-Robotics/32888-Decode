b314eb1cbe56979492f553534f9944ee325ce0dd
anshpatel20202009-creator <anshpatel20202009@gmail.com>
2026-01-14 15:58:21 -0800

    ready to test field centric

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/TeleOpLinearFieldCentric.java
@@ -0,0 +1,335 @@
+/* Copyright (c) 2021 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.OpMode;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+/*
+ * This file contains an example of a Linear "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When a selection is made from the menu, the corresponding OpMode is executed.
+ *
+ * This particular OpMode illustrates driving a 4-motor Omni-Directional (or Holonomic) robot.
+ * This code will work with either a Mecanum-Drive or an X-Drive train.
+ * Both of these drives are illustrated at https://gm0.org/en/latest/docs/robot-design/drivetrains/holonomic.html
+ * Note that a Mecanum drive must display an X roller-pattern when viewed from above.
+ *
+ * Also note that it is critical to set the correct rotation direction for each motor.  See details below.
+ *
+ * Holonomic drives provide the ability for the robot to move in three axes (directions) simultaneously.
+ * Each motion axis is controlled by one Joystick axis.
+ *
+ * 1) Axial:    Driving forward and backward               Left-joystick Forward/Backward
+ * 2) Lateral:  Strafing right and left                     Left-joystick Right and Left
+ * 3) Yaw:      Rotating Clockwise and counter clockwise    Right-joystick Right and Left
+ *
+ * This code is written assuming that the right-side motors need to be reversed for the robot to drive forward.
+ * When you first test your robot, if it moves backward when you push the left stick forward, then you must flip
+ * the direction of all 4 motors (see code below).
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ */
+
+// Based on the sample: Basic: Omni Linear OpMode
+@TeleOp(name = "TeleOp Control", group = "Teleop")
+
+public class TeleOpLinearFieldCentric extends LinearOpMode {
+
+    // Declare OpMode members for each of the 4 motors.
+    private ElapsedTime runtime = new ElapsedTime();
+    private ElapsedTime catatime = new ElapsedTime();
+
+    // Declare drive motors
+    private DcMotor leftFrontDrive = null;
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor rightBackDrive = null;
+
+    private IMU imu = null;
+
+    // Declare end-effector members
+    private DcMotor intake = null;
+    private DcMotor catapult1 = null;
+    private DcMotor catapult2 = null;
+    private DcMotor foot = null;
+
+    // motor power 1 = 100% and 0.5 = 50%
+    // negative values = reverse ex: -0.5 = reverse 50%
+    private double INTAKE_IN_POWER = 1.0;
+    private double INTAKE_OUT_POWER = -0.9;
+    private double INTAKE_OFF_POWER = 0.0;
+    private double intakePower = INTAKE_OFF_POWER;
+
+    private double FOOT_UP_POWER = 1.0;
+    private double FOOT_DOWN_POWER = -0.85;
+    private double FOOT_OFF_POWER = 0.0;
+    private double footPower = FOOT_OFF_POWER;
+
+    private double CATAPULT_UP_POWER = -1.0;
+    private double CATAPULT_DOWN_POWER = 1.0;
+    private double CATAPULT_HOLD_POWER = 0.2;
+
+    private enum CatapultModes {UP, DOWN, HOLD}
+    private CatapultModes pivotMode;
+
+    private enum FootMode {UP, DOWN, BRAKE}
+    private FootMode footmode;
+
+    /*
+     * Code to run ONCE when the driver hits INIT (same as previous year's init())
+     */
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+
+        // Initialize the hardware variables. Note that the strings used here must correspond
+        // to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "left_front_drive");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "left_back_drive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
+
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        catapult1 = hardwareMap.get(DcMotor.class, "catapult1");
+        catapult2 = hardwareMap.get(DcMotor.class, "catapult2");
+        foot = hardwareMap.get(DcMotor.class, "foot");
+
+        // ########################################################################################
+        // !!!            IMPORTANT Drive Information. Test your motor directions.            !!!!!
+        // ########################################################################################
+        // Most robots need the motors on one side to be reversed
+        // to drive forward.
+        // The motor reversals shown here are for a "direct drive" robot (the wheels turn the same direction as the motor shaft)
+        // If your robot has additional gear reductions or uses a right-angled drive, it's important to ensure
+        // that your motors are turning in the correct direction.  So, start out with the reversals here, BUT
+        // when you first test your robot, push the left joystick forward and observe the direction the wheels turn.
+        // Reverse the direction (flip FORWARD <-> REVERSE ) of any wheel that runs backward
+        // Keep testing until ALL the wheels move the robot forward when you push the left joystick forward.
+
+        // set direction of wheel motors
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+
+
+        imu = hardwareMap.get(IMU.class, "imu");
+        // This needs to be changed to match the orientation on your robot
+        RevHubOrientationOnRobot.LogoFacingDirection logoDirection =
+                RevHubOrientationOnRobot.LogoFacingDirection.BACKWARD;
+        RevHubOrientationOnRobot.UsbFacingDirection usbDirection =
+                RevHubOrientationOnRobot.UsbFacingDirection.UP;
+
+        RevHubOrientationOnRobot orientationOnRobot = new
+                RevHubOrientationOnRobot(logoDirection, usbDirection);
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
+
+
+
+        // set direction of subsystem motors
+        intake.setDirection(DcMotor.Direction.FORWARD); // Forward should INTAKE.
+        catapult1.setDirection(DcMotor.Direction.REVERSE); // Backwards should pivot DOWN, or in the stowed position.
+        catapult2.setDirection(DcMotor.Direction.FORWARD);
+        foot.setDirection(DcMotor.Direction.REVERSE); // Backwards should should stay UP, or in the stowed position
+
+        // set initial subsystem behavior
+        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        catapult1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        catapult2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        foot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Wait for the game to start (driver presses START)
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+        catatime.reset();
+        // run until the end of the match (driver presses STOP)
+        // same as previous year's loop() code
+        double leftFrontPower = 0;
+        double rightFrontPower = 0;
+        double leftBackPower = 0;
+        double rightBackPower = 0;
+        while (opModeIsActive()) {
+            double max;
+
+            // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
+            //axial = speed, lateral = turn, yaw = strafe
+            double axial = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
+            double lateral = -gamepad1.right_stick_x;
+            double yaw = -gamepad1.left_stick_x;
+
+            boolean intakeInButton = gamepad2.left_trigger > 0.2;
+            boolean intakeOutButton = gamepad2.left_bumper;
+
+            // This conditional reduces ambiguity when multiple buttons are pressed.
+            if (intakeInButton && intakeOutButton) {
+                intakeInButton = false;
+            }
+
+            boolean footOutButton = gamepad2.a;
+            boolean footUpButton = gamepad2.b;
+            if (footOutButton && footUpButton) {
+                footOutButton = false;
+            }
+
+            boolean catapultUpButton = gamepad2.right_bumper;
+            boolean catapultDownButton = gamepad2.right_trigger > 0.2;
+            if (catapultUpButton && catapultDownButton) {
+                catapultUpButton = false;
+            }
+
+
+            // INTAKE CODE
+            if (intakeInButton) {
+                intakePower = INTAKE_IN_POWER;
+            } else if (intakeOutButton) {
+                intakePower = INTAKE_OUT_POWER;
+            } else {
+                intakePower = INTAKE_OFF_POWER;
+            }
+
+            // FOOT CODE
+            if (footOutButton) {
+                footmode = FootMode.DOWN;
+                footPower = FOOT_DOWN_POWER;
+            } else if (footUpButton) {
+                footmode = FootMode.UP;
+                footPower = FOOT_UP_POWER;
+            } else {
+                footmode = FootMode.BRAKE;
+                footPower = FOOT_OFF_POWER;
+            }
+
+            // Determine pivot mode
+            if (catapultUpButton) {
+                pivotMode = CatapultModes.UP;
+                catapult1.setPower(CATAPULT_UP_POWER);
+                catapult2.setPower(CATAPULT_UP_POWER);
+            } else if (catapultDownButton) {
+                pivotMode = CatapultModes.DOWN;
+                catapult1.setPower(CATAPULT_DOWN_POWER);
+                catapult2.setPower(CATAPULT_DOWN_POWER);
+            } else {
+                pivotMode = CatapultModes.HOLD;
+                catapult1.setPower(CATAPULT_HOLD_POWER);
+                catapult2.setPower(CATAPULT_HOLD_POWER);
+                //Slight feed forward to keep catapult down while driving
+            }
+
+
+            intake.setPower(intakePower);
+            foot.setPower(footPower);
+
+            String catapult_mode_str;
+            if (pivotMode == CatapultModes.UP) {
+                catapult_mode_str = "UP";
+            } else if (pivotMode == CatapultModes.DOWN) {
+                catapult_mode_str = "DOWN";
+            } else {
+                catapult_mode_str = "HOLD";
+            }
+
+            driveFieldRelative(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+
+
+            // UPDATE TELEMETRY
+            // Show the elapsed game time, wheel power, and other systems power
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
+            telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
+            telemetry.addData("Intake", "%%4.2f", intake.getPower());
+            telemetry.addData("Foot Power", "%4.2f", foot.getPower());
+            telemetry.addData("Foot MODE", "%s", footmode);
+            telemetry.addData("Catapult1 Current/Target/power", "%d, %d, %4.2f",
+                    catapult1.getCurrentPosition(), catapult1.getTargetPosition(), catapult1.getPower());
+            telemetry.addData("Catapult2 Current/Target/power", "%d, %d, %4.2f",
+                    catapult2.getCurrentPosition(), catapult2.getTargetPosition(), catapult2.getPower());
+            telemetry.addData("Catapult MODE", "%s", catapult_mode_str);
+            telemetry.update();
+        }
+    }
+
+    private void driveFieldRelative(double forward, double right, double rotate) {
+        // First, convert direction being asked to drive to polar coordinates
+        double theta = Math.atan2(forward, right);
+        double r = Math.hypot(right, forward);
+
+        // Second, rotate angle by the angle the robot is pointing
+        theta = AngleUnit.normalizeRadians(theta -
+                imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
+
+        // Third, convert back to cartesian
+        double newForward = r * Math.sin(theta);
+        double newRight = r * Math.cos(theta);
+
+        // Finally, call the drive method with robot relative forward and right amounts
+        drive(newForward, newRight, rotate);
+    }
+
+    // Thanks to FTC16072 for sharing this code!!
+    public void drive(double forward, double right, double rotate) {
+        // This calculates the power needed for each wheel based on the amount of forward,
+        // strafe right, and rotate
+        double frontLeftPower = forward + right + rotate;
+        double frontRightPower = forward - right - rotate;
+        double backRightPower = forward + right - rotate;
+        double backLeftPower = forward - right + rotate;
+
+        double maxPower = 1.0;
+        double maxSpeed = 1.0;  // make this slower for outreaches
+
+        // This is needed to make sure we don't pass > 1.0 to any wheel
+        // It allows us to keep all of the motors in proportion to what they should
+        // be and not get clipped
+        maxPower = Math.max(maxPower, Math.abs(frontLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(frontRightPower));
+        maxPower = Math.max(maxPower, Math.abs(backRightPower));
+        maxPower = Math.max(maxPower, Math.abs(backLeftPower));
+
+        // We multiply by maxSpeed so that it can be set lower for outreaches
+        // When a young child is driving the robot, we may not want to allow full
+        // speed.
+        leftFrontDrive.setPower(maxSpeed * (frontLeftPower / maxPower));
+        rightFrontDrive.setPower(maxSpeed * (frontRightPower / maxPower));
+        leftBackDrive.setPower(maxSpeed * (backLeftPower / maxPower));
+        rightBackDrive.setPower(maxSpeed * (backRightPower / maxPower));
+    }
+}
