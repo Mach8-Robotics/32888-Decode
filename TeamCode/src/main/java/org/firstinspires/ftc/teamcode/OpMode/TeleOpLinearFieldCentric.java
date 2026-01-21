8c0afcc4bc1acadb6778ca0f96301e463abe6e0f
anshpatel20202009-creator <anshpatel20202009@gmail.com>
2026-01-21 14:48:01 -0800

    added field centric drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/TeleOpLinearFieldCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/TeleOpLinearFieldCentric.java
@@ -28,13 +28,14 @@
  */
 
 package org.firstinspires.ftc.teamcode.OpMode;
-
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.IMU;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 /*
  * This file contains an example of a Linear "OpMode".
  * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
@@ -64,9 +65,9 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  */
 
 // Based on the sample: Basic: Omni Linear OpMode
-@TeleOp(name = "Tomcat: Everybot TeleOp robot centric", group = "Teleop")
+@TeleOp(name = "tomcat: evrybot TeleOp Control field centric", group = "Teleop")
 
-public class TeleOpControlLinearOpMode extends LinearOpMode {
+public class TeleOpLinearFieldCentric extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
     private ElapsedTime runtime = new ElapsedTime();
@@ -77,6 +78,8 @@ public class TeleOpControlLinearOpMode extends LinearOpMode {
     private DcMotor leftBackDrive = null;
     private DcMotor rightFrontDrive = null;
     private DcMotor rightBackDrive = null;
+
+    private IMU imu = null;
 
     // Declare end-effector members
     private DcMotor intake = null;
@@ -145,6 +148,20 @@ public class TeleOpControlLinearOpMode extends LinearOpMode {
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
 
+
+        imu = hardwareMap.get(IMU.class, "imu");
+        // This needs to be changed to match the orientation on your robot
+        RevHubOrientationOnRobot.LogoFacingDirection logoDirection =
+                RevHubOrientationOnRobot.LogoFacingDirection.BACKWARD;
+        RevHubOrientationOnRobot.UsbFacingDirection usbDirection =
+                RevHubOrientationOnRobot.UsbFacingDirection.UP;
+
+        RevHubOrientationOnRobot orientationOnRobot = new
+                RevHubOrientationOnRobot(logoDirection, usbDirection);
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
+
+
+
         // set direction of subsystem motors
         intake.setDirection(DcMotor.Direction.FORWARD); // Forward should INTAKE.
         catapult1.setDirection(DcMotor.Direction.REVERSE); // Backwards should pivot DOWN, or in the stowed position.
@@ -176,9 +193,8 @@ public class TeleOpControlLinearOpMode extends LinearOpMode {
             // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
             //axial = speed, lateral = turn, yaw = strafe
             double axial = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
-            double lateral = gamepad1.right_stick_x;
-            double yaw = gamepad1.left_stick_x;
-            // for tomcat i think
+            double lateral = -gamepad1.right_stick_x;
+            double yaw = -gamepad1.left_stick_x;
 
             boolean intakeInButton = gamepad2.left_trigger > 0.2;
             boolean intakeOutButton = gamepad2.left_bumper;
@@ -200,40 +216,6 @@ public class TeleOpControlLinearOpMode extends LinearOpMode {
                 catapultUpButton = false;
             }
 
-            // DRIVE CODE
-            // Combine the joystick requests for each axis-motion to determine each wheel's power.
-            // Set up a variable for each drive wheel to save the power level for telemetry.
-            leftFrontPower = axial + lateral + yaw;
-            rightFrontPower = axial - lateral - yaw;
-            leftBackPower = axial - lateral + yaw;
-            rightBackPower = axial + lateral - yaw;
-
-            // Normalize the values so no wheel power exceeds 100%
-            // This ensures that the robot maintains the desired motion.
-            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-            max = Math.max(max, Math.abs(leftBackPower));
-            max = Math.max(max, Math.abs(rightBackPower));
-
-            if (max > 1.0) {
-                leftFrontPower /= max;
-                rightFrontPower /= max;
-                leftBackPower /= max;
-                rightBackPower /= max;
-            }
-
-            // This is wheel test code
-            // Uncomment the following code to test your motor directions.
-            // Each button should make the corresponding motor run FORWARD.
-            //   1) First get all the motors to take to correct positions on the robot
-            //      by adjusting your Robot Configuration if necessary.
-            //   2) Then make sure they run in the correct direction by modifying the
-            //      the setDirection() calls above.
-            // Once the correct motors move in the correct direction re-comment this code.
-
-/*            leftFrontPower  = gamepad1.x ? 1.0 : 0.0;  // X gamepad
-            leftBackPower   = gamepad1.a ? 1.0 : 0.0;  // A gamepad
-            rightFrontPower = gamepad1.y ? 1.0 : 0.0;  // Y gamepad
-            rightBackPower  = gamepad1.b ? 1.0 : 0.0;  // B gamepad */
 
             // INTAKE CODE
             if (intakeInButton) {
@@ -272,11 +254,6 @@ public class TeleOpControlLinearOpMode extends LinearOpMode {
                 //Slight feed forward to keep catapult down while driving
             }
 
-            // WRITE EFFECTORS - Send calculated power to wheels
-            leftFrontDrive.setPower(leftFrontPower);
-            rightFrontDrive.setPower(rightFrontPower);
-            leftBackDrive.setPower(leftBackPower);
-            rightBackDrive.setPower(rightBackPower);
 
             intake.setPower(intakePower);
             foot.setPower(footPower);
@@ -289,6 +266,9 @@ public class TeleOpControlLinearOpMode extends LinearOpMode {
             } else {
                 catapult_mode_str = "HOLD";
             }
+
+            driveFieldRelative(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+
 
             // UPDATE TELEMETRY
             // Show the elapsed game time, wheel power, and other systems power
@@ -305,5 +285,51 @@ public class TeleOpControlLinearOpMode extends LinearOpMode {
             telemetry.addData("Catapult MODE", "%s", catapult_mode_str);
             telemetry.update();
         }
+    }
+
+    private void driveFieldRelative(double forward, double right, double rotate) {
+        // First, convert direction being asked to drive to polar coordinates
+        double theta = Math.atan2(forward, right);
+        double r = Math.hypot(right, forward);
+
+        // Second, rotate angle by the angle the robot is pointing
+        theta = AngleUnit.normalizeRadians(theta -
+                imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
+
+        // Third, convert back to cartesian
+        double newForward = r * Math.sin(theta);
+        double newRight = r * Math.cos(theta);
+
+        // Finally, call the drive method with robot relative forward and right amounts
+        drive(newForward, newRight, rotate);
+    }
+
+    // Thanks to FTC16072 for sharing this code!!
+    public void drive(double forward, double right, double rotate) {
+        // This calculates the power needed for each wheel based on the amount of forward,
+        // strafe right, and rotate
+        double frontLeftPower = forward + right + rotate;
+        double frontRightPower = forward - right - rotate;
+        double backRightPower = forward + right - rotate;
+        double backLeftPower = forward - right + rotate;
+
+        double maxPower = 1.0;
+        double maxSpeed = 1.0;  // make this slower for outreaches
+
+        // This is needed to make sure we don't pass > 1.0 to any wheel
+        // It allows us to keep all of the motors in proportion to what they should
+        // be and not get clipped
+        maxPower = Math.max(maxPower, Math.abs(frontLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(frontRightPower));
+        maxPower = Math.max(maxPower, Math.abs(backRightPower));
+        maxPower = Math.max(maxPower, Math.abs(backLeftPower));
+
+        // We multiply by maxSpeed so that it can be set lower for outreaches
+        // When a young child is driving the robot, we may not want to allow full
+        // speed.
+        leftFrontDrive.setPower(maxSpeed * (frontLeftPower / maxPower));
+        rightFrontDrive.setPower(maxSpeed * (frontRightPower / maxPower));
+        leftBackDrive.setPower(maxSpeed * (backLeftPower / maxPower));
+        rightBackDrive.setPower(maxSpeed * (backRightPower / maxPower));
     }
 }
