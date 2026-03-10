7b5c9eacdfa226c01795b85b19677ace48694de3
WynnTheMan <wynnston.burke@gmail.com>
2026-03-10 18:57:30 -0400

    UPDATE

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/Auto/RedGoal9BallShooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/Auto/RedGoal9BallShooter.java
@@ -47,10 +47,10 @@ public class RedGoal9BallShooter extends AutoBase{
     public void buildpaths() {
         path0 = new Path(new BezierCurve(new Pose(24.000, 122.000).mirror(), new Pose(26.000, 120.000).mirror()));
         path0.setLinearHeadingInterpolation(Math.toRadians(36), Math.toRadians(36));
-        path1 = new Path(new BezierCurve(new Pose(26.000, 120.000).mirror(), new Pose(48.000, 96.000).mirror()));
+        path1 = new Path(new BezierCurve(new Pose(26.000, 120.000).mirror(), new Pose(96.000, 96.000)));
         path1.setLinearHeadingInterpolation(Math.toRadians(36), Math.toRadians(36));
-        path2 = new Path(new BezierCurve(new Pose(48.000, 96.000).mirror(), new Pose(48.000, 60.000).mirror()));
-        path2.setLinearHeadingInterpolation(Math.toRadians(36), Math.toRadians(0));
+        path2 = new Path(new BezierCurve(new Pose(96.000, 96.000), new Pose(48.000, 60.000).mirror()));
+        path2.setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0));
         path3 = new Path(new BezierCurve(new Pose(48.000,60.000).mirror(), new Pose(12.000, 55.000).mirror()));
         path3.setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0));
         path4 = new Path(new BezierCurve(new Pose(12.000, 55.000).mirror(), new Pose(36.000, 60.000).mirror()));
